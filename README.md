5029b1e61b08f7c5fdee28b924402ae96b4a7b06
Melvyn <42531205+melvyn9@users.noreply.github.com>
2022-11-22 21:25:12 -0800

    Given IDs to buttons

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab 9 - Starter
+Name: Melvyn Nam Qiang Tan
+PID: A16412682
---
+++ index.js
@@ -0,0 +1,16 @@
+//Console Log
+document.getElementById("consoleLog").addEventListener
+//Console Error
+//Console Count
+//Console Warn
+//Console Assert
+//Console Clear
+//Console Dir
+//Console Dirxml
+//Console Start Group
+//Console End Group
+//Console Table
+//Console Start Timer
+//Console End Timer
+//Console Trace
+//Console Global
